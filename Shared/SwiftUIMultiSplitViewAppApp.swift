fa69443f5339c9104556b3780f540fe3624d9353
Holger Mayer <holgermayer@icloud.com>
2020-07-19 20:36:14 +0200

    Initial Commit

---
+++ Shared/SwiftUIMultiSplitViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMultiSplitViewAppApp.swift
+//  Shared
+//
+//  Created by Holger Mayer on 19.07.20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMultiSplitViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
