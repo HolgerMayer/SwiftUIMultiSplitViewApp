1bfa89deb787317eb5e7655c29047412f4b1817b
Holger Mayer <holgermayer@icloud.com>
2020-07-22 08:07:07 +0200

    Changed Detailed view so first line won't scroll

--- Shared/SplitviewFrontend/Views/ItemDetailView.swift
+++ Shared/SplitviewFrontend/Views/ItemDetailView.swift
@@ -16,22 +16,23 @@ struct ItemDetailView: View {
     }
     
     var body: some View {
-        ScrollView {
+        VStack{
             if item != nil {
                 Text("Detail View of \(item!.name)" )
                     .foregroundColor(model.userData.color)
             } else {
                 Text("Please select item")
             }
-            
-            if loc != nil {
-                Text("\(loc!.description)" )
-                    .padding()
+            ScrollView {
+             
+               if loc != nil {
+                  Text("\(loc!.description)" )
+                      .padding()
                 
-            }
+              }
             
+            }
         }.frame(minWidth: 300, idealWidth: 400, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: 100, idealHeight: 500, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-
         .navigationBarTitle(item?.name ?? "Please select an item")
     }
 }
